7e46bfb6e95df23d4168931f1d7b2e535db82a69
hellowners <89602061+hellowners@users.noreply.github.com>
2025-06-14 18:50:17 +0530

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+//https://flowing-warthog-0.clerk.accounts.dev
+export default {
+  providers: [
+    {
+      domain: "https://flowing-warthog-0.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
